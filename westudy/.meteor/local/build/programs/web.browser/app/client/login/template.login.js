fe80db84ffcdc66158436c0c35bdc650a4dc5896
Anton Ulyanov <antonulyanov@outlook.com>
2015-12-02 16:20:22 -0500

    Revert "Added tracking for Meteor files"
    This reverts commit 2af1fd29d82b8204c85c4d2fd5cb4f98e3af9277.

--- westudy/.meteor/local/build/programs/web.browser/app/client/login/template.login.js
+++
@@ -1,8 +0,0 @@
-(function(){
-Template.__checkName("login");
-Template["login"] = new Template("Template.login", (function() {
-  var view = this;
-  return HTML.Raw('<form class="login">\n        <p>Email: <input type="email" name="email" required=""></p>\n        <p>Password: <input type="password" name="password" required=""></p>\n        <p><input type="submit" value="Login"></p>\n    </form>');
-}));
-
-}).call(this);
--- westudy/.meteor/local/build/programs/web.browser/app/client/register/template.register.js
+++
@@ -1,8 +0,0 @@
-(function(){
-Template.__checkName("register");
-Template["register"] = new Template("Template.register", (function() {
-  var view = this;
-  return HTML.Raw('<form>\n        <p>Email: <input type="email" name="email"></p>\n        <p>Password: <input type="password" name="password"></p>\n        <p><input type="submit" value="Register"></p>\n    </form>');
-}));
-
-}).call(this);

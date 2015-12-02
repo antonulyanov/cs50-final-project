2f2daf6a6cb5098ae65079472f9f4a0c38434bdd
Anton Ulyanov <antonulyanov@outlook.com>
2015-12-02 00:04:23 -0500

    A new beginning...

--- westudy/client/westudy.js
+++ westudy/client/westudy.js
@@ -1,23 +1,10 @@
-// counter starts at 0
-Session.setDefault('counter', 0);
-
-Template.hello.helpers({
-    counter: function () {
-        return Session.get('counter');
-    }
-});
-
-Template.hello.events({
-    'click button': function () {
-        // increment the counter when button is clicked
-        Session.set('counter', Session.get('counter') + 1);
-    }
-});
+// Router Configuration
 
 Router.configure({
     layoutTemplate: 'home'
 });
 
 Router.route('/', {
+    name: 'home',
     template: 'home'
 });

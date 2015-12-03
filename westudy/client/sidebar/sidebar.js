9f675544259b2200a83301a9fcd1d85b39d1383c
Nick Hawke <n.hawke96@gmail.com>
2015-12-02 23:47:30 -0500

    updated sidebar to highlight active item

--- westudy/client/sidebar/sidebar.js
+++ westudy/client/sidebar/sidebar.js
@@ -1,7 +1,18 @@
-console.log("fuck youself");
+Template.sidebar.rendered = function() {
+
+    var currentPage = Router.current().route.getName();
+
+    $("#" + currentPage).addClass("active");
+
+};
+
 
 Template.sidebar.events({
-    "click .nav a": function () {
-        console.log("clicked");
+    "click .nav a": function (event, template) {
+        // set clicked on tab to be active
+        console.log("click");
+
+        $(".active").removeClass("active");
+        $(event.target).parent().addClass("active");
     }
 });

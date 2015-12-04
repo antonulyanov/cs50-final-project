cd1d44347e520baa26b0416477ee60dea0c09fbc
Nick Hawke <n.hawke96@gmail.com>
2015-12-03 20:52:09 -0500

    FINALLY THE FUCKING SIDEBAR WORKS AS INTENDED

--- westudy/client/views/sidebar/sidebar.js
+++ westudy/client/views/sidebar/sidebar.js
@@ -1,9 +1,24 @@
-// highlight proper tab on sidebar
+// actions to be performed after router brings up page
 Router.onAfterAction(function() {
+
+    // highlight proper tab
     var currentPage = Router.current().route.getName();
 
     $(".active").removeClass("active");
     $("#" + currentPage).addClass("active");
+
+    // hide/unhide things based on if user is logged in
+    var loggedIn = $(".loggedIn");
+    var loggedOut = $(".loggedOut");
+
+    if (Meteor.userId() !== null) {
+        loggedIn.removeClass("hidden");
+        loggedOut.addClass("hidden");
+    }
+    else {
+        loggedOut.removeClass("hidden");
+        loggedIn.addClass("hidden");
+    }
 
 });
 

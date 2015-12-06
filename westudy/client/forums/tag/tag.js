de57dfc64cbe76197c18f82c3cf30d793b3ab4cb
Nick Hawke <n.hawke96@gmail.com>
2015-12-06 02:05:54 -0500

    changed file names to match convention

--- westudy/client/forums/tag/tag.js
+++
@@ -1,9 +0,0 @@
-Template.forums_tag.helpers({
-    posts : function() {
-        return getPosts();
-    }
-});
-
-function getPosts() {
-    return Posts.find({},{sort : {timestamp : -1}}).fetch();
-}
